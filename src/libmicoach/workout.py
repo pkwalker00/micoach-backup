d3039e1f19091f009e7d45fdb5f47a4fc81f83d0
Patrick Walker <pkwalker00@gmail.com>
2013-06-22 21:55:49 -0700

    changed workoutlist content

--- src/libmicoach/workout.py
+++ src/libmicoach/workout.py
@@ -23,36 +23,28 @@ class WorkoutList(object):
     
     def __init__(self, data):
         self.content = []
-        self.log = []
         for w in data.iter(xa.nodestring(data, 'WorkoutLog')):
             start = datetime.strptime(w.find(xa.findstring(data, 'StartDate')).text[:19], '%Y-%m-%dT%H:%M:%S')
             end = datetime.strptime(w.find(xa.findstring(data, 'StopDate')).text[:19], '%Y-%m-%dT%H:%M:%S')
-        
-            d = int(w.find(xa.findstring(data, 'Distance')).find(xa.findstring(data, 'Value')).text)
-            if d > 1000:
-                distance = '%.1f km' % (d/1000.)
-            else:
-                distance = '%d m' % (d)
+            duration = end - start
+            
+            distance = int(w.find(xa.findstring(data, 'Distance')).find(xa.findstring(data, 'Value')).text)
             id = int(w.find(xa.findstring(data, 'WorkoutId')).text)
             name = w.find(xa.findstring(data, 'Name')).text
             activity = w.find(xa.findstring(data, 'ActivityType')).text
             type = w.find(xa.findstring(data, 'CompletedWorkoutType')).text
-            time = str(end-start)
             hr = int(w.find(xa.findstring(data, 'AvgHR')).find(xa.findstring(data, 'Value')).text)
             pace = float(w.find(xa.findstring(data, 'AvgPace')).find(xa.findstring(data, 'Value')).text)
             
-            self.log.append([id, name, str(start)[:10], str(start)[-8:][:5],  activity, type, time, distance, hr, str('{0:.2f}'.format(round(pace, 2)))])
-            
-            self.content.append({'id': id,
-                                 'name': name,
-                                 'date': str(start),
-                                 'activity': activity,
-                                 'type': type,
-                                 'time': time,
-                                 'distance': distance,
-                                 'hr': hr,
-                                 'pace': pace
-                                })
+            self.content.append({'id':id, 
+                            'name': name, 
+                            'start': start, 
+                            'activity': activity, 
+                            'type': type, 
+                            'duration': duration, 
+                            'distance': distance, 
+                            'hr': hr, 
+                            'pace': pace})
 
     def __len__(self):
         return len(self.content)
@@ -64,6 +56,5 @@ class WorkoutList(object):
     def __repr__(self):
         return 'WorkoutList: contains (%d) workouts' % (len(self.content))
 
-    def display(self):
-        for w in self.content:
-            print('{:d}\t{:s}\t{:s}\{:s}\{:s}\t{:s}\t{:s}\t{:d}\t{:n}'.format(w['id'],w['name'],w['date'],w['activity'],w['type'],w['time'],w['distance'],w['hr'],w['pace']))
+    def get_Content(self):
+        return self.content
