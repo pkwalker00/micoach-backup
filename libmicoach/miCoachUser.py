50cc5be4b0bdff725a0e0d3e52867b51f592db69
Patrick Walker <pkwalker00@gmail.com>
2014-03-11 22:23:01 -0700

    Udated to use Journal class

--- libmicoach/miCoachUser.py
+++ libmicoach/miCoachUser.py
@@ -1,36 +1,29 @@
 import requests, json
+from libmicoach.journal import *
 
 class miCoachUser(object):
-
-    cookies = ''
-    loggedin = False
     
     def __init__(self):
         self.url = 'https://micoach.adidas.com'
+        self.cookies = ''
+        self.loggedin = False
     
     def login(self, user_email, user_password):
         login_url = '/us/services/login/loginuser'
         creds = {'email':user_email,'password':user_password,'errors':{}}
         login_request = requests.post(self.url+login_url, data=creds)
+        self.journal = ''
         if login_request.status_code == 200:
             authtoken = login_request.cookies['micoach_authtoken']
-            miCoachUser.cookies=dict(micoach_authtoken=authtoken)
-            miCoachUser.loggedin = True
-            print("Login Successful")
-            print("Retrieving Journal")
-            self.journal = self.getJournal()
-            print("Complete")
+            self.cookies=dict(micoach_authtoken=authtoken)
+            self.loggedin = True
 
     def getJournal(self):
-        journal_url = '/us//Track/JournalData'
-        journal_sort = {"page":1,"start":0,"limit":1000,"sort":[{"property":"startDateTime","direction":"desc"}]}
-        journal_request = requests.post(self.url+journal_url, data = journal_sort, cookies=miCoachUser.cookies)
-        journal_list = json.loads(journal_request.text)
-        return journal_list
+        self.journal = Journal(self.cookies)
 
     def getWorkout(self, workoutId):
         workout_url = '/us/services/track/getChartWorkoutDetail?completedWorkoutId='
-        workout_request = requests.get(self.url + workout_url + str(workoutId), cookies=miCoachUser.cookies)
+        workout_request = requests.get(self.url + workout_url + str(workoutId), cookies=self.cookies)
         workout_data = json.loads(workout_request.text)
         del workout_data['details']['WorkoutInfo']
         return workout_data
