40bb439e0780f0f4f016deed97c3be49b638d674
Patrick Walker <pkwalker00@gmail.com>
2014-03-15 12:46:35 -0700

    Updated to use workout from json

--- libmicoach/tcx.py
+++ libmicoach/tcx.py
@@ -1,18 +1,14 @@
 from datetime import datetime, timedelta
 from dateutil.parser import parse
-import dateutil.tz 
-import libmicoach.xmlassist as xa
+import dateutil.tz
 from lxml import etree
 
-def writeTcx(filename, content):
+def writeTcx(filename, workout):
     """Convert miCoach xml to TCX format"""
-	
-    #parse xml content with lxml
-    xml = content
-	
-    utc = xa.search(xml, 'StartDateTimeUTC')
 
-    local = xa.search(xml, 'StartDateTime')
+    utc = workout['WorkoutInfo']['StartDateTimeUTC'][:-1]
+
+    local = workout['WorkoutInfo']['StartDateTime'][:-1]
 
     #Set UTC start time for workout
     start = datetime.strptime(utc, "%Y-%m-%dT%H:%M:%S")	
@@ -24,8 +20,26 @@ def writeTcx(filename, content):
     if parse(local + datetz).timetuple().tm_isdst != 0:
         start = start - timedelta(hours=1)
 
-    gps_active = xa.search(xml, 'GPSActive')
-    hr_active = int(xa.findvalue(xml, 'AvgHR'))
+    #Find what sensors were used
+    if 'GPSActive' in workout['WorkoutInfo']:
+        gps_active = workout['WorkoutInfo']['GPSActive']
+    else:
+        gps_active = False
+    hrm_active = 'Value' in workout['WorkoutInfo']['AvgHR']
+    footpod_active = 'Value' in workout['WorkoutInfo']['AvgStrideRate']
+    
+    #Determine ActivityType from code
+    activityType = workout['WorkoutInfo']['ActivityType']
+    if activityType == 1:
+        workoutActivity = 'Running'
+    if activityType == 2:
+        workoutActivity = 'Walking'
+    if activityType == 3:
+        workoutActivity = 'Cycling'
+    if activityType == 14:
+        workoutActivity = 'Nordic Skiing'
+    if activityType == 999:
+        workoutActivity = 'Other'
 
     #create tcx container
     xmlns = 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2'
@@ -34,40 +48,47 @@ def writeTcx(filename, content):
     NSMAP = {None: xmlns, 'xsi': xsi, 'xsd': xsd}
 
     tcx = etree.Element('TrainingCenterDatabase', nsmap=NSMAP) 
-	
+
     activities = etree.SubElement(tcx, 'Activities')
     activity = etree.SubElement(activities, 'Activity')
-    etree.SubElement(activity, 'Sport').text = xa.search(xml, 'ActivityType')
+    etree.SubElement(activity, 'Sport').text = workoutActivity
     etree.SubElement(activity, 'Id').text = str(start)
-	
+
     lap = etree.SubElement(activity, 'Lap')
     lap.set('StartTime', str(start))
-    etree.SubElement(lap, 'TotalTimeSeconds').text = xa.search(xml,'TotalTime')
-    etree.SubElement(lap, 'DistanceMeters').text = xa.findvalue(xml, 'TotalDistance') 
-    etree.SubElement(lap, 'Calories').text = xa.findvalue(xml, 'TotalCalories')
-    if hr_active != 0:
+    etree.SubElement(lap, 'TotalTimeSeconds').text = str(workout['WorkoutInfo']['TotalTime'])
+    etree.SubElement(lap, 'DistanceMeters').text = str(workout['WorkoutInfo']['TotalDistance']['Value'] )
+    etree.SubElement(lap, 'Calories').text = str(workout['WorkoutInfo']['TotalCalories']['Value'])
+    
+    if hrm_active:
         avghrbpm = etree.SubElement(lap, 'AverageHeartRateBpm')
-        etree.SubElement(avghrbpm, 'Value').text = xa.findvalue(xml, 'AvgHR')
+        etree.SubElement(avghrbpm, 'Value').text = str(workout['WorkoutInfo']['AvgHR']['Value'])
         maxhrbpm = etree.SubElement(lap, 'MaximumHeartRateBpm')
-        etree.SubElement(maxhrbpm, 'Value').text = xa.findvalue(xml, 'PeakHR')
+        etree.SubElement(maxhrbpm, 'Value').text = str(workout['WorkoutInfo']['PeakHR']['Value'])
+
+    if footpod_active:
+        avgcadence = etree.SubElement(lap,  'AverageCadence')
+        etree.SubElement(avgcadence, 'Value').text = str(workout['WorkoutInfo']['AvgStrideRate']['Value'])
     
     #Setup basic Track	
     track = etree.SubElement(lap,'Track')
 
     #Add GPS and/or HRM data points
-    for point in xml.iter(xa.nodestring(xml, 'CompletedWorkoutDataPoint')):
-        delta = timedelta(0, float(point.find(xa.findstring(xml, 'TimeFromStart')).text))
+    for point in workout['CompletedWorkoutDataPoints']:
+        delta = timedelta(0, point['TimeFromStart'])
         trackpoint = etree.SubElement(track, 'Trackpoint')
-        etree.SubElement(trackpoint, 'Time').text  = (start + delta).isoformat()
-        if gps_active == "true":			
+        etree.SubElement(trackpoint, 'Time').text  = (start + delta).strftime("%Y-%m-%dT%H:%M:%SZ")
+        if gps_active:			        
             position = etree.SubElement(trackpoint, 'Position')
-            etree.SubElement(position, 'LatitudeDegrees').text = point.find(xa.findstring(xml, 'Latitude')).text
-            etree.SubElement(position, 'LongitudeDegrees').text = point.find(xa.findstring(xml, 'Longitude')).text
-            etree.SubElement(trackpoint, 'AltitudeMeters').text = point.find(xa.findstring(xml, 'Altitude')).text
-            etree.SubElement(trackpoint, 'DistanceMeters').text = point.find(xa.findstring(xml, 'Distance')).text
-        if hr_active != 0:
+            etree.SubElement(position, 'LatitudeDegrees').text = str(point['Latitude'])
+            etree.SubElement(position, 'LongitudeDegrees').text = str(point['Longitude'])
+            etree.SubElement(trackpoint, 'AltitudeMeters').text = str(point['Altitude'])
+        if footpod_active:
+            etree.SubElement(trackpoint, 'DistanceMeters').text = str(point['Distance'])
+            etree.SubElement(trackpoint, 'Cadence').text = str(point['StrideRate'])
+        if hrm_active:
             hrbpm = etree.SubElement(trackpoint, 'HeartRateBpm', attrib={'{'+xsi+'}type': 'HeartRateInBeatsPerMinute_t'})
-            etree.SubElement(hrbpm, 'Value').text = point.find(xa.findstring(xml, 'HeartRate')).text
+            etree.SubElement(hrbpm, 'Value').text = str(point['HeartRate'])
 
     #Write Completed tcx file
     etree.ElementTree(tcx).write(filename, xml_declaration=True, encoding='utf-8', pretty_print=True)
