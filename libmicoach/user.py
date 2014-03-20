1973b601b804bed655345b0456c86f18f1630af8
Patrick Walker <pkwalker00@gmail.com>
2014-03-20 09:55:19 -0700

    begun adding exception handling for login method

--- libmicoach/user.py
+++ libmicoach/user.py
@@ -19,14 +19,16 @@ class miCoachUser(object):
     def login(self, user_email, user_password):
         url = 'https://micoach.adidas.com/us/services/login/loginuser'
         creds = {'email':user_email,'password':user_password,'errors':{}}
-        login_request = requests.post(url, data=creds)
-        if login_request.status_code == 200:
+        try:
+            login_request = requests.post(url, data=creds, timeout=5)
             authtoken = login_request.cookies['micoach_authtoken']
             self.cookies=dict(micoach_authtoken=authtoken)
             self.loggedin = True
             threading.Thread(target = self.getJournal).start()
             self.getUserID()
-    
+        except:
+            return "Login failed"
+
     def logout(self):
         self.cookies = ''
         self.loggedin = False
