80a22d1548cb0f7ba957d7506be13fe797277691
Patrick Walker <pkwalker00@gmail.com>
2014-03-26 17:47:00 -0700

    added Worker/Thread for login progress

--- micoach-backup.py
+++ micoach-backup.py
@@ -12,6 +12,12 @@ class miCoachWindow(QtGui.QWidget, Ui_Form):
         self.user = libmicoach.user.miCoachUser()
         self.setTableModel()
         self.loginButton.clicked.connect(self.login)
+        self.thread = Thread()
+        self.worker = Worker()
+        self.worker.moveToThread(self.thread)
+        self.thread.start()
+        self.worker.loginComplete.connect(self.loggedIn)
+        self.worker.loginFailed.connect(self.loginFail)
 
     def setTableModel(self):
         self.model = JournalTableModel(self.journalData)
@@ -33,16 +39,42 @@ class miCoachWindow(QtGui.QWidget, Ui_Form):
             QtGui.QMessageBox.warning(self, "No Password", "You must enter a password")
             self.passwordEdit.setFocus()
             return
-        try:
-            self.user.login(self.emailEdit.text(), self.passwordEdit.text())
-            self.journalData = self.user.journalList()
-            self.setTableModel()
-        except:
-            QtGui.QMessageBox.warning(self, "Login Failed", "Login Failed:Please try again")
-            self.emailEdit.setText('')
-            self.emailEdit.setFocus()
-            self.passwordEdit.setText('')
-            self.user.logout()
+        self.progressBar.setRange(0, 0)
+        QtCore.QMetaObject.invokeMethod(self.worker, 'login', QtCore.Qt.QueuedConnection, QtCore.Q_ARG(libmicoach.user.miCoachUser, self.user), QtCore.Q_ARG(str, self.emailEdit.text()), QtCore.Q_ARG(str, self.passwordEdit.text()))
+
+    def loginFail(self):
+        self.progressBar.setRange(0, 1)
+        QtGui.QMessageBox.warning(self, "Login Failed", "Login Failed:Please try again")
+        self.emailEdit.setText('')
+        self.emailEdit.setFocus()
+        self.passwordEdit.setText('')
+        self.user.logout()
+
+    def loggedIn(self):
+        self.progressBar.setRange(0, 1)
+        self.journalData = self.user.journalList()
+        self.setTableModel()
+
+class Worker(QtCore.QObject):
+    loginComplete = QtCore.pyqtSignal()
+    loginFailed = QtCore.pyqtSignal()
+
+    @QtCore.pyqtSlot(libmicoach.user.miCoachUser, str, str)
+    def login(self, user, email, password):
+        user.login(email, password)
+        if user.loggedin:
+            self.loginComplete.emit()
+        else:
+            self.loginFailed.emit()
+            return
+    
+class Thread(QtCore.QThread):
+    def __init__(self, parent=None):
+        QtCore.QThread.__init__(self, parent)
+    def start(self):
+        QtCore.QThread.start(self)
+    def run(self):
+        QtCore.QThread.run(self)
 
 if __name__ == "__main__":
     import sys
